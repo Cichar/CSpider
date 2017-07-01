b315e328ece28d1cc2a7b1a207363b9d965e7952
Cichar <363655056@qq.com>
2017-07-01 22:59:35 +0800

    initial ZhiHu Spider

---
+++ Spider/Zhihu.py
@@ -0,0 +1,12 @@
+# -*- coding:utf-8 -*-
+
+from Utils.BaseSpider import BaseSpider
+
+__Author__ = 'Cichar'
+__Email__ = '363655056@qq.com'
+__CreateDate__ = '2017/7/1'
+__Version__ = '0.1'
+
+
+class ZhiHuSpider(BaseSpider):
+    name = u'知乎用户爬虫'
