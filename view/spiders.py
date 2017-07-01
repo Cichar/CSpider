7f01fc74d7762e5252f922aa92ed777d1197b82c
Cichar <363655056@qq.com>
2017-07-02 04:31:15 +0800

    add spiders modules

---
+++ view/spiders.py
@@ -0,0 +1,18 @@
+# -*- coding:utf-8 -*-
+
+from tornado import gen, web
+
+from Utils.BaseHandle import BaseHandler
+from Manager.SpiderManager import SpiderManager
+
+__Author__ = 'Cichar'
+__Email__ = '363655056@qq.com'
+__CreateDate__ = '2017/7/2'
+__Version__ = '0.1'
+
+
+class SpidersHandler(BaseHandler):
+    @gen.coroutine
+    def get(self):
+        spiders = SpiderManager().spiders
+        self.render("spiders.html", spiders=spiders)
