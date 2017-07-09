1579126b42a9fcfa86426d8cc8b3d5f5de5ac873
Cichar <363655056@qq.com>
2017-07-09 23:02:31 +0800

    add a test task to Spider.ZhiHu

--- Spider/Zhihu.py
+++ Spider/Zhihu.py
@@ -1,6 +1,7 @@
 # -*- coding:utf-8 -*-
 
 from Utils.BaseSpider import BaseSpider
+from Utils.SpiderWorkers import spider_worker
 
 __Author__ = 'Cichar'
 __Email__ = '363655056@qq.com'
@@ -10,3 +11,11 @@ __Version__ = '0.1'
 
 class ZhiHuSpider(BaseSpider):
     name = u'ZhiHuUser'
+
+    def task_distribute(self, data):
+        self.add.delay(data['target'])
+
+    @staticmethod
+    @spider_worker.task
+    def add(x):
+        return x * 2
