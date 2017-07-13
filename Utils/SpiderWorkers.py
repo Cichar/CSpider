2366ef44f827022ddc7ebfab5540d5a389c4b158
Cichar <363655056@qq.com>
2017-07-13 21:42:22 +0800

    add celery configuration

--- Utils/SpiderWorkers.py
+++ Utils/SpiderWorkers.py
@@ -13,10 +13,10 @@ spider_worker.conf.update(
     # TimeZone
     CELERY_TIMEZONE='Asia/Shanghai',
     CELERY_ENABLE_UTC=True,
-    # Serializer
-    CELERY_ACCEPT_CONTENT=['json'],
-    CELERY_TASK_SERIALIZER='json',
-    CELERY_RESULT_SERIALIZER='json',
+    # # Serializer
+    # CELERY_ACCEPT_CONTENT=['json'],
+    # CELERY_TASK_SERIALIZER='json',
+    # CELERY_RESULT_SERIALIZER='json',
     # Schedule Task
     # cmd: celery beat -A Utils.SpiderWorkers -l info
     CELERYBEAT_SCHEDULE={
@@ -26,16 +26,24 @@ spider_worker.conf.update(
             'args': ('23',),
         },
     },
-    # Task Queue
-    CELERY_QUEUES={
-        Queue("default", Exchange("default", type='direct'), routing_key="for_default"),
-    },
     # Default Task Configuration
     CELERY_DEFAULT_QUEUE='default',
     CELERY_DEFAULT_EXCHANGE='default',
     CELERY_DEFAULT_ROUTING_KEY='for_default',
-    # Task Rounte
-    CELERY_ROUTES = {
-        '': {'queue': 'default', 'routing_key': 'for_default'},
+    # Task Queue
+    CELERY_QUEUES=(
+        Queue("default", Exchange("default", type='direct'), routing_key="for_default"),
+        Queue("long_task_1", Exchange("long_task_1", type='direct'), routing_key="for_long_task_1"),
+        Queue("long_task_2", Exchange("long_task_2", type='direct'), routing_key="for_long_task_2"),
+        Queue("long_task_3", Exchange("long_task_3", type='direct'), routing_key="for_long_task_3"),
+        Queue("long_task_4", Exchange("long_task_4", type='direct'), routing_key="for_long_task_4"),
+        Queue("short_task_1", Exchange("short_task_1", type='direct'), routing_key="for_short_task_1"),
+        Queue("short_task_2", Exchange("short_task_2", type='direct'), routing_key="for_short_task_2"),
+        Queue("short_task_3", Exchange("short_task_3", type='direct'), routing_key="for_short_task_3"),
+        Queue("short_task_4", Exchange("short_task_4", type='direct'), routing_key="for_short_task_4"),
+    ),
+    # Task Route
+    CELERY_ROUTES={
+        'default': {'queue': 'default', 'routing_key': 'for_default'},
     }
 )
