5b9fe0bc6eb35b7795a4b405f196a7acb97f100a
Cichar <363655056@qq.com>
2017-07-13 21:40:08 +0800

    add ZhiHuUserInfo Model

--- Database/models.py
+++ Database/models.py
@@ -1,7 +1,38 @@
 # -*- coding:utf-8 -*-
 
-import time
+from sqlalchemy import Column, String, Integer, DateTime, TEXT, Boolean
 
-from . import Base, DataBase
+from . import Base, db
 
-db = DataBase()
+
+class ZhiHuUserInfo(Base):
+    """
+
+    ZhiHu User Info Model
+
+    """
+
+    __tablename__ = 'zhhuserinfos'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(64), default=u'未知')
+    headline = Column(String(128), default=u'未知')
+    location = Column(String(64), default=u'未知')
+    description = Column(TEXT(), default=u'未知')
+    voteup_count = Column(Integer)
+    thanked_count = Column(Integer)
+    favorited_count = Column(Integer)
+    logs_count = Column(Integer)
+    question_count = Column(Integer)
+    url_token = Column(String(64), default=u'未知')
+    follower_count = Column(Integer)
+    following_count = Column(Integer)
+    business = Column(String(64), default=u'未知')
+    employment = Column(String(64), default=u'未知')
+    company = Column(String(64), default=u'未知')
+    update_time = Column(DateTime())
+    updata_flag = Column(Boolean, default=True)
+    crawl_flag = Column(Boolean, default=False)
+
+    def __repr__(self):
+        return 'ZhiHuUser %r' % self.name
