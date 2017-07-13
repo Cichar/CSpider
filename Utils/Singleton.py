ae7f3739578ea3e85e7faf0453dd741e54855e6f
Cichar <363655056@qq.com>
2017-07-13 23:30:59 +0800

    add a singleton metaclass base on abc.ABCmeta

--- Utils/Singleton.py
+++ Utils/Singleton.py
@@ -1,5 +1,7 @@
 # -*- coding:utf-8 -*-
 
+import abc
+
 __Author__ = 'Cichar'
 __Email__ = '363655056@qq.com'
 __CreateDate__ = '2017/7/1'
@@ -7,7 +9,7 @@ __Version__ = '0.1'
 
 
 class Singleton(type):
-    """ 单例设计模式所用元类 """
+    """ Singleton metaclass """
 
     def __init__(cls, *args, **kwargs):
         super(Singleton, cls).__init__(*args, **kwargs)
@@ -19,12 +21,14 @@ class Singleton(type):
         return cls.__instance
 
 
-class Test(object, metaclass=Singleton):
-    pass
+class ABC(abc.ABCMeta):
+    """ Singleton metaclass base on abc.ABCMeta """
 
+    def __init__(cls, name, bases, class_dict):
+        super(ABC, cls).__init__(name, bases, class_dict)
+        cls._instance = None
 
-if __name__ == '__main__':
-    a = Test()
-    b = Test()
-    print(id(a))
-    print(id(b))
+    def __call__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = super(ABC, cls).__call__(*args, **kwargs)
+        return cls._instance
