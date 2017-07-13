3b0cd7a0452c0a2270216d75d4c261b8e09daff7
Cichar <363655056@qq.com>
2017-07-13 21:42:35 +0800

    verify code

--- view/task.py
+++ view/task.py
@@ -12,9 +12,13 @@ __Version__ = '0.1'
 
 class SpiderTaskHandler(BaseHandler):
     @gen.coroutine
-    def get(self, spider):
+    def get(self, spider_name):
+        """ Choice The Spider Task Form Depend On Spider Name, 
+            Then Pass The Form Object To The Web.
+        """
+
         try:
-            form = SpiderManager().spiders[spider]['form']()
+            form = SpiderManager().spiders[spider_name]['form']()
         except Exception as err:
             print(err)
         else:
@@ -22,10 +26,16 @@ class SpiderTaskHandler(BaseHandler):
 
     @gen.coroutine
     def post(self, spider_name):
+        """ Choice The Spider Depend On Spider Name, 
+            Then Pass The Form Data Which Given By User On Web To The Selected Spider, 
+            The Spider Uses Form Data To Distribute Tasks.
+        """
+
         try:
             spider_dict = SpiderManager().spiders[spider_name]
             form = spider_dict['form'](self.request.arguments)
             spider_dict['spider'].task_distribute(form.data)
-            self.write(form.data)
         except Exception as err:
             print(err)
+        else:
+            self.write(form.data)
