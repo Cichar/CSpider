820610ae4d5ad06e81d255e80c45d8513e472038
Cichar <363655056@qq.com>
2017-07-16 23:13:38 +0800

    rewrite error handle

--- Utils/BaseHandle.py
+++ Utils/BaseHandle.py
@@ -1,6 +1,6 @@
 # -*- coding:utf-8 -*-
 
-from tornado.web import RequestHandler
+import tornado.web
 
 __Author__ = 'Cichar'
 __Email__ = '363655056@qq.com'
@@ -12,23 +12,34 @@ __Version__ = '0.1'
 """
 
 
-class BaseHandler(RequestHandler):
+class BaseHandler(tornado.web.RequestHandler):
     def data_received(self, chunk):
         pass
+
+    def write_error(self, status_code, **kwargs):
+        self.db.session.close()
+        if status_code in [403, 404, 500]:
+            self.render('error\%d.html' % status_code)
+        else:
+            self.render('error\error.html', status_code=status_code)
 
     @property
     def db(self):
         return self.application.db
 
     def on_finish(self):
-        """ 一个请求完成后调用此方法 """
+        """ On Request Finish, Close The Database Session """
         try:
             pass
         finally:
             self.db.session.close()
 
     def query(self, model):
-        """ 查询数据库 """
+        """ Database Query Function """
 
         _model = self.db.session.query(model)
         return _model
+
+
+class ErrorHandler(tornado.web.ErrorHandler, BaseHandler):
+    pass
