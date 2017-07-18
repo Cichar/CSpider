95d4f2decd471ca53ea81959b73df714d933d570
Cichar <363655056@qq.com>
2017-07-18 17:20:19 +0800

    add hostname

--- workers.bat
+++ workers.bat
@@ -1,4 +1,4 @@
-start celery -A Utils.SpiderWorkers worker -l warning -c 1 -Q default
-start celery -A Utils.SpiderWorkers worker -l warning -c 1 -Q short_task_1
-start celery -A Utils.SpiderWorkers worker -l warning -c 1 -Q long_task_1
-start celery -A Utils.SpiderWorkers worker -l warning -c 1 -Q long_task_2
+start celery -A Utils.SpiderWorkers worker -l warning -c 1 -Q default --hostname default_host
+start celery -A Utils.SpiderWorkers worker -l warning -c 1 -Q short_task_1 --hostname short_host_1
+start celery -A Utils.SpiderWorkers worker -l warning -c 1 -Q long_task_1 --hostname long_host_1
+start celery -A Utils.SpiderWorkers worker -l warning -c 1 -Q long_task_2 --hostname long_host_2
