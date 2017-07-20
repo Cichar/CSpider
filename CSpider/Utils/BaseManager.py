984d7ad73272a2ff78b18e5eb5292ab557ff6b86
Cichar <363655056@qq.com>
2017-07-21 00:10:36 +0800

    create BaseManager Class

---
+++ CSpider/Utils/BaseManager.py
@@ -0,0 +1,19 @@
+# -*- utf-8 -*-
+
+import abc
+
+from Utils.Singleton import ABC
+
+__Author__ = 'Cichar'
+__Email__ = '363655056@qq.com'
+__CreateDate__ = '2017/7/20'
+__version__ = '0.1'
+
+
+class BaseManager(object, metaclass=ABC):
+    def __init__(self):
+        pass
+
+    @abc.abstractmethod
+    def _create_dict(self):
+        pass
