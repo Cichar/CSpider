c6a9d2e30292ca13b52388db19bf622528a3b53e
Cichar <363655056@qq.com>
2017-07-21 12:17:59 +0800

    fix bug which cause the Celery Worker can't find the Spider Task

--- CSpider/Spider/__init__.py
+++ CSpider/Spider/__init__.py
@@ -1,1 +1,15 @@
 # -*- coding:utf-8 -*-
+
+import os
+import glob
+
+wrong_file = ['__init__']
+
+try:
+    for module_file in glob.glob(os.path.join(os.path.dirname(__file__), '*.py')):
+        module_name, _ = os.path.splitext(os.path.basename(module_file))
+        if module_name not in wrong_file:
+            # print(module_name)
+            __import__('Spider.' + module_name)
+except Exception as err:
+    print(err)
