2132fb842025512a723ba52e6508e1eebebdbb9c
Cichar <363655056@qq.com>
2017-07-23 12:59:51 +0800

    add a cmd to support the common task's queue

--- CSpider/workers.bat
+++ CSpider/workers.bat
@@ -2,3 +2,4 @@ start celery -A Utils.SpiderWorkers worker -l warning -c 1 -Q default --hostname
 start celery -A Utils.SpiderWorkers worker -l warning -c 1 -Q short_task_1 --hostname short_host_1
 start celery -A Utils.SpiderWorkers worker -l warning -c 1 -Q long_task_1 --hostname long_host_1
 start celery -A Utils.SpiderWorkers worker -l warning -c 1 -Q long_task_2 --hostname long_host_2
+start celery -A Utils.SpiderWorkers worker -l warning -c 1 -Q common_task_1 --hostname common_host_1
