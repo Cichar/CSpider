ee9259704a64d7da34fea1f9f570ee11c49c280e
Cichar <363655056@qq.com>
2017-07-28 19:46:29 +0800

    add a worker to support short_task_2 queue

--- CSpider/workers.bat
+++ CSpider/workers.bat
@@ -1,5 +1,6 @@
 start celery -A Utils.SpiderWorkers worker -l warning -c 1 -Q default --hostname default_host
 start celery -A Utils.SpiderWorkers worker -l warning -c 1 -Q short_task_1 --hostname short_host_1
+start celery -A Utils.SpiderWorkers worker -l warning -c 1 -Q short_task_2 --hostname short_host_2
 start celery -A Utils.SpiderWorkers worker -l warning -c 1 -Q long_task_1 --hostname long_host_1
 start celery -A Utils.SpiderWorkers worker -l warning -c 1 -Q long_task_2 --hostname long_host_2
 start celery -A Utils.SpiderWorkers worker -l warning -c 1 -Q common_task_1 --hostname common_host_1
