c2dcbb24ba6bcab34f812c7f6516b428fc5dfc81
Cichar <363655056@qq.com>
2017-07-30 21:12:23 +0800

    add note and verify update_title function

--- CSpider/Cecharts/BaseChart.py
+++ CSpider/Cecharts/BaseChart.py
@@ -22,6 +22,25 @@ class BaseChart(object):
 
     def __init__(self, title, subtitle=None, toolbox=False, name='myChart',
                  width='100%', height='400px', render_id=None):
+        """
+        :param title: 
+                    Chart's Title.
+        :param subtitle: 
+                    Charts's subtitle.
+        :param toolbox: 
+                    Show Or Not Show Chart's Toolbox. Default Is ''False''.
+        :param name: 
+                    The Name For Render In Web Page. Default Is ''myChart''.
+                    User Need To Provide Another Name To Support When The 
+                    Web Page Has More Than One Chart.
+        :param width: 
+                    The Width For Render In Web Page. Default Is ''100%''.
+        :param height: 
+                    The Height For Render In Web Page. Default Is ''400px''.
+        :param render_id: 
+                    The Id For JS To Select, Then ECharts Initial The Object.
+        """
+
         if not name or not isinstance(name, str):
             raise ValueError('name is required and must be string type.')
         if not render_id or not isinstance(render_id, str):
@@ -43,19 +62,57 @@ class BaseChart(object):
             getattr(self, func)()
 
     def _add_title(self, title, subtitle):
-        """ Add The Title And Subtitle For The Cecharts """
+        """ Add The Title And Subtitle For The Chart """
 
         self._option['title'] = {"text": title}
         if subtitle:
             self._option['title'].update({"subtext": subtitle})
 
-    def update_title(self, **kwargs):
+    def update_title(self, show=None, link=None, target=None, text_color=None, text_font_style=None,
+                     text_font_weight=None, text_font_family=None, text_font_size=None,text_align=None,
+                     text_base_line=None, sub_link=None, sub_target=None, sub_text_style=None,
+                     padding=None, item_gap=None, z_level=None, z=None, left=None, top=None, right=None,
+                     bottom=None, background_color=None, border_color=None, border_width=None, shadow_blur=None,
+                     shadow_color=None, shadow_offset_x=None, shadow_offset_y=None):
         """ Update The Title Configuration """
 
-        self._option['title'].update(kwargs)
+        title_option = {
+            'show': show,
+            'link': link,
+            'target': target,
+            'textStyle': {
+                'color': text_color,
+                'fontStyle': text_font_style,
+                'fontWeight': text_font_weight,
+                'fontFamily': text_font_family,
+                'fontSize': text_font_size,
+            },
+            'textAlign': text_align,
+            'textBaseline': text_base_line,
+            'sublink': sub_link,
+            'subtarget': sub_target,
+            'subtextStyle': sub_text_style,
+            'padding': padding,
+            'itemGap': item_gap,
+            'zlevel': z_level,
+            'z': z,
+            'left': left,
+            'top': top,
+            'right': right,
+            'bottom': bottom,
+            'backgroundColor': background_color,
+            'borderColor': border_color,
+            'borderWidth': border_width,
+            'shadowBlur': shadow_blur,
+            'shadowColor': shadow_color,
+            'shadowOffsetX': shadow_offset_x,
+            'shadowOffsetY': shadow_offset_y
+        }
+
+        self._update_option('title', title_option)
 
     def _add_toolbox(self):
-        """ Add The Toolbox For The Cecharts """
+        """ Add The Toolbox For The Chart """
 
         self._option['toolbox'] = {"left": "right",
                                    "feature": {
@@ -69,7 +126,7 @@ class BaseChart(object):
         self._option['toolbox'].update(kwargs)
 
     def _add_tooltip(self):
-        """ Add The Tooltip For The Cecharts """
+        """ Add The Tooltip For The Chart """
 
         self._option['tooltip'] = {'trigger': 'axis'}
 
@@ -78,7 +135,7 @@ class BaseChart(object):
         pass
 
     def _add_legend(self):
-        """ Add The Legend For The Cecharts """
+        """ Add The Legend For The Chart """
 
         self._option['legend'] = {'data': []}
 
@@ -120,8 +177,27 @@ class BaseChart(object):
     def update_series(self, datas, legend=True):
         raise NotImplementedError()
 
+    def _update_option(self, key, option):
+        """ Used To Update The Option In ''self._option[key]'' """
+
+        if not isinstance(key, str):
+            raise TypeError('key must be string type, get {0} type.'.format(type(key).__name__))
+        if not isinstance(option, dict):
+            raise TypeError('option must be dict type, get {0} type.'.format(type(option).__name__))
+
+        for k, v in option.items():
+            if v:
+                if isinstance(v, dict):
+                    for _k, _v in v.items():
+                        if _v:
+                            if not self._option[key].get(k, None):
+                                self._option[key].update({k: {}})
+                            self._option[key][k].update({_k: _v})
+                else:
+                    self._option[key].update({k: v})
+
     def render(self):
-        """ Render Cecharts """
+        """ Render Chart """
 
         if not self._option.get('yAxis'):
             self.update_y_axis()
@@ -134,5 +210,5 @@ class BaseChart(object):
 
 if __name__ == '__main__':
     a = BaseChart('Test', render_id='test', toolbox=True)
-    a.update_title(top='center')
+    a.update_title()
     print(a)
