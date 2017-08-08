b9cec53d90c8f4e9bc2330b4f3922c74c10148dc
Cichar <363655056@qq.com>
2017-08-08 17:58:35 +0800

    verify Polar Option

--- CSpider/Htmlrender/Cecharts/options/polar.py
+++ CSpider/Htmlrender/Cecharts/options/polar.py
@@ -1,6 +1,7 @@
 # -*- utf-8 -*-
 
 from .base_option import BaseOption
+from .tooltip import ToolTip
 from Htmlrender.decorator import check_args
 
 __Author__ = 'Cichar'
@@ -10,6 +11,16 @@ __Version__ = '0.1'
 
 
 class Polar(BaseOption):
+    def __init__(self):
+        self.zlevel = None
+        self.z = None
+        self.center = None
+        self.radius = None
+        self.tooltip = ToolTip()
+
     @check_args
-    def set_keys(self, *args, **kwargs):
-        pass
+    def set_keys(self, z_level: int=None, z: int=None, center: list=None, radius: list=None):
+        self.zlevel = z_level
+        self.z = z
+        self.center = center
+        self.radius = radius
