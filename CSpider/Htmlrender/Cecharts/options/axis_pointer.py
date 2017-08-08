561a94e6b840aeb957d0292fa094d4da3d8aa1bc
Cichar <363655056@qq.com>
2017-08-08 17:58:08 +0800

    verify axisPointer Option

--- CSpider/Htmlrender/Cecharts/options/axis_pointer.py
+++ CSpider/Htmlrender/Cecharts/options/axis_pointer.py
@@ -1,7 +1,11 @@
 # -*- utf-8 -*-
 
 from .base_option import BaseOption
-from .base_option import TextStyle
+from .base_option import Label
+from .base_option import LineStyle
+from .base_option import ShadowStyle
+from .base_option import CrossStyle
+from .base_option import Handle
 from Htmlrender.decorator import check_args
 
 __Author__ = 'Cichar'
@@ -29,103 +33,28 @@ class AxisPointer(BaseOption):
         self.z = z
 
 
-class Label(BaseOption):
+class AxisPointer2(BaseOption):
+    """ This Class Is For xAxis, yAxis, RadiusAxis """
+
     def __init__(self):
         self.show = None
-        self.precision = None
-        self.formatter = None
-        self.margin = None
-        self.textStyle = TextStyle()
-        self.padding = None
-        self.backgroundColor = None
-        self.borderColor = None
-        self.borderWidth = None
-        self.shadowBlur = None
-        self.shadowColor = None
-        self.shadowOffsetX = None
-        self.shadowOffsetY = None
+        self.type = None
+        self.snap = None
+        self.z = None
+        self.label = Label()
+        self.lineStyle = LineStyle()
+        self.shadowStyle = ShadowStyle()
+        self.triggerTooltip = None
+        self.value = None
+        self.status = None
+        self.handle = Handle()
 
-    @check_args
-    def set_keys(self, show: bool=None, precision=None, formatter=None, margin: int=None, padding=None,
-                 background_color: str=None, border_color: str=None, border_width: str=None, shadow_blur: int=None,
-                 shadow_color: str=None, shadow_offset_x: int=None, shadow_offset_y: int=None):
+    def set_keys(self, show: bool=None, _type: str=None, snap: bool=None, z: int=None,
+                 trigger_tooltip: bool=None, value: int=None, status: bool=None):
         self.show = show
-        self.precision = precision
-        self.formatter = formatter
-        self.margin = margin
-        self.padding = padding
-        self.backgroundColor = background_color
-        self.borderColor = border_color
-        self.borderWidth = border_width
-        self.shadowBlur = shadow_blur
-        self.shadowColor = shadow_color
-        self.shadowOffsetX = shadow_offset_x
-        self.shadowOffsetY = shadow_offset_y
-
-
-class LineStyle(BaseOption):
-    def __init__(self):
-        self.color = None
-        self.width = None
-        self.type = None
-        self.shadowBlur = None
-        self.shadowColor = None
-        self.shadowOffsetX = None
-        self.shadowOffsetY = None
-        self.opacity = None
-
-    @check_args
-    def set_keys(self, color: str=None, width: int=None, _type: str=None, shadow_blur: int=None,
-                 shadow_color: str=None, shadow_offset_x: int=None, shadow_offset_y: int=None, opacity: int=None):
-        self.color = color
-        self.width = width
         self.type = _type
-        self.shadowBlur = shadow_blur
-        self.shadowColor = shadow_color
-        self.shadowOffsetX = shadow_offset_x
-        self.shadowOffsetY = shadow_offset_y
-        self.opacity = opacity
-
-
-class ShadowStyle(BaseOption):
-    def __init__(self):
-        self.color = None
-        self.shadowBlur = None
-        self.shadowColor = None
-        self.shadowOffsetX = None
-        self.shadowOffsetY = None
-        self.opacity = None
-
-    @check_args
-    def set_keys(self, color: str=None, shadow_blur: int=None, shadow_color: str=None, shadow_offset_x: int=None,
-                 shadow_offset_y: int=None, opacity: int=None):
-        self.color = color
-        self.shadowBlur = shadow_blur
-        self.shadowColor = shadow_color
-        self.shadowOffsetX = shadow_offset_x
-        self.shadowOffsetY = shadow_offset_y
-        self.opacity = opacity
-
-
-class CrossStyle(BaseOption):
-    def __init__(self):
-        self.color = None
-        self.width = None
-        self.type = None
-        self.shadowBlur = None
-        self.shadowColor = None
-        self.shadowOffsetX = None
-        self.shadowOffsetY = None
-        self.opacity = None
-
-    @check_args
-    def set_keys(self, color: str=None, width: int=None, _type: str=None, shadow_blur: int=None,
-                 shadow_color: str=None, shadow_offset_x: int=None, shadow_offset_y: int=None, opacity: int=None):
-        self.color = color
-        self.width = width
-        self.type = _type
-        self.shadowBlur = shadow_blur
-        self.shadowColor = shadow_color
-        self.shadowOffsetX = shadow_offset_x
-        self.shadowOffsetY = shadow_offset_y
-        self.opacity = opacity
+        self.snap = snap
+        self.z = z
+        self.triggerTooltip = trigger_tooltip
+        self.value = value
+        self.status = status
