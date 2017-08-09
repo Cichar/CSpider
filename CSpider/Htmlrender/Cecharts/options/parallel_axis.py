e4b34e50fe0179899c10f7ae6fcc3164fd4ced6d
Cichar <363655056@qq.com>
2017-08-09 21:38:01 +0800

    verify code

--- CSpider/Htmlrender/Cecharts/options/parallel_axis.py
+++ CSpider/Htmlrender/Cecharts/options/parallel_axis.py
@@ -1,6 +1,7 @@
 # -*- utf-8 -*-
 
-from .base_option import BaseOption
+from Htmlrender.Cecharts.options.base_option import ParallelAxisDefault
+from Htmlrender.Cecharts.options.base_option import AreaSelectStyle
 from Htmlrender.decorator import check_args
 
 __Author__ = 'Cichar'
@@ -9,7 +10,36 @@ __CreateDate__ = '2017/8/7'
 __Version__ = '0.1'
 
 
-class ParallelAxis(BaseOption):
+class ParallelAxis(ParallelAxisDefault):
+    def __init__(self):
+        super().__init__()
+        self.dim = None
+        self.parallelIndex = None
+        self.realtime = None
+        self.areaSelectStyle = AreaSelectStyle()
+
     @check_args
-    def set_keys(self, *args, **kwargs):
-        pass
+    def set_keys(self, dim: int=None, parallel_index: int=None, realtime: bool=None, _type: str=None,
+                 name: str=None, name_location: str=None, name_gap: int=None, name_rotate: int=None,
+                 inverse: bool=None, boundary_gap=None, _min=None, _max=None, scale: bool=None,
+                 split_num: int=None, min_interval: int=None, interval: int=None, log_base: int=None,
+                 silent: bool=None, trigger_event: bool=None):
+        self.dim = dim
+        self.parallelIndex = parallel_index
+        self.realtime = realtime
+        self.type = _type
+        self.name = name
+        self.nameLocation = name_location
+        self.nameGap = name_gap
+        self.nameRotate = name_rotate
+        self.inverse = inverse
+        self.boundaryGap = boundary_gap
+        self.min = _min
+        self.max = _max
+        self.scale = scale
+        self.splitNumber = split_num
+        self.minInterval = min_interval
+        self.interval = interval
+        self.logBase = log_base
+        self.silent = silent
+        self.triggerEvent = trigger_event
