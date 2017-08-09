733724bed7a98abfc49205f218263dfb1312d35b
Cichar <363655056@qq.com>
2017-08-09 17:56:48 +0800

    create a new child class

--- CSpider/Htmlrender/Cecharts/options/axis_pointer.py
+++ CSpider/Htmlrender/Cecharts/options/axis_pointer.py
@@ -15,6 +15,8 @@ __Version__ = '0.1'
 
 
 class AxisPointer(BaseOption):
+    """ This Class Is For Grid, ToolTip """
+
     def __init__(self):
         self.type = None
         self.axis = None
@@ -34,7 +36,7 @@ class AxisPointer(BaseOption):
 
 
 class AxisPointer2(BaseOption):
-    """ This Class Is For xAxis, yAxis, RadiusAxis """
+    """ This Class Is For xAxis, yAxis, RadiusAxis, AngleAxis """
 
     def __init__(self):
         self.show = None
@@ -58,3 +60,25 @@ class AxisPointer2(BaseOption):
         self.triggerTooltip = trigger_tooltip
         self.value = value
         self.status = status
+
+
+class AxisPointer3(AxisPointer2):
+    """ This Class Is For Main AxisPinter """
+
+    def __init__(self):
+        super().__init__()
+        self.link = None
+        self.triggerOn = None
+
+    def set_keys(self, show: bool=None, _type: str=None, snap: bool=None, z: int=None,
+                 trigger_tooltip: bool=None, value: int=None, status: bool=None, link: list=None,
+                 trigger_on: str=None):
+        self.show = show
+        self.type = _type
+        self.snap = snap
+        self.z = z
+        self.triggerTooltip = trigger_tooltip
+        self.value = value
+        self.status = status
+        self.link = link
+        self.triggerOn = trigger_on
