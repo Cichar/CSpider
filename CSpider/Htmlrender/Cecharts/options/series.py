fe9cfe58d48f39af181996fab4218bc8de20fbcd
Cichar <363655056@qq.com>
2017-08-16 13:41:34 +0800

    change base class

--- CSpider/Htmlrender/Cecharts/options/series.py
+++ CSpider/Htmlrender/Cecharts/options/series.py
@@ -1,7 +1,6 @@
 # -*- utf-8 -*-
 
-from .base_option import BaseOption
-from Htmlrender.decorator import check_args
+from .base_option import BaseOption2
 
 __Author__ = 'Cichar'
 __Email__ = '363655056@qq.com'
@@ -9,7 +8,5 @@ __CreateDate__ = '2017/8/7'
 __Version__ = '0.1'
 
 
-class Series(BaseOption):
-    @check_args
-    def set_keys(self, *args, **kwargs):
-        pass
+class Series(BaseOption2):
+    pass
